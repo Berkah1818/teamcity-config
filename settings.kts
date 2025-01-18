1e6f820dbfed08ac1698b9c7a07286a6b5d6e98f
Berkah1818 <berkahmelimpahkayaraya@gmail.com>
2025-01-18 10:09:14 +0800

    Add .gitignore to exclude temporary files.

---
+++ settings.kts
@@ -0,0 +1,43 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.ScriptBuildStep
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+
+version = "2022.04"
+
+project {
+    description = "Contains all other projects"
+    buildType(DastardlyScan)
+}
+object DastardlyScan : BuildType({
+    name = "Dastardly from Burp Suite Scan"
+    vcs {
+        cleanCheckout = true
+    }
+    features {
+        feature {
+            type = "xml-report-plugin"
+            param("xmlReportParsing.reportType", "junit")
+            param("xmlReportParsing.reportDirs", "+:**/reports/**.xml")
+        }
+    }
+    steps {
+        script {
+            name = "Dastardly from Burp Suite Scan"
+            dockerImage = "public.ecr.aws/portswigger/dastardly:latest"
+            dockerPull = true
+            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
+            dockerRunParameters = """
+                -e BURP_START_URL=https://ginandjuice.shop/
+                -e BURP_REPORT_FILE_PATH=%teamcity.build.checkoutDir%/reports/dastardly-report.xml
+            """.trimIndent()
+            scriptContent = """
+                mkdir -p %system.teamcity.build.workingDir%/reports
+                docker-entrypoint.sh dastardly
+            """.trimIndent()
+        }
+
+    }
+    artifactRules = """
+        reports/** => reports
+    """.trimIndent()
+})
